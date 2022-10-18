7f0e40fba42220fdfe3cd0a61981779fa2bd9463
NisrinaAmaliaIf <nisrinaamaliaiffatunnisa@gmail.com>
2022-10-18 10:53:06 +0700

    endpoint

--- ebook/routes/api.php
+++ ebook/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//ga ada edit dan create
+Route::get('me', function(){
+    
+});
+
+Route::get('me', [AuthController::class, 'me']);
--- ebook/routes/web.php
+++ ebook/routes/web.php
@@ -17,6 +17,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//ini string
 Route::get('coba', function(){
     return "hallo nisrin";
+});
+
+//array
+Route::get('coba1', function(){
+    return ['aku', 'kamu'];
+});
+
+//object json
+Route::get('coba2', function(){
+    return[
+        "Nama" => "Nisrina A",
+        "Kelas" => "XII RPL 5",
+        "NIS" => 123456
+    ];
+});
+
+//OBJEK JSON
+Route::get('coba3', function(){
+    return response()->json(
+        [
+            "Nama" => "Nisrina A",
+            "Kelas" => "XII RPL 5",
+            "NIS" => 123456
+        ], 201
+        );
 });
